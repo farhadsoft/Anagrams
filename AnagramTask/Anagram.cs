01ea596b03971da820f53e1f7452fac1749d533b
Farhad <epam@farhad.su>
2020-12-30 13:31:01 +0300

    All done!

--- AnagramTask/Anagram.cs
+++ AnagramTask/Anagram.cs
@@ -1,9 +1,13 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace AnagramTask
 {
     public class Anagram
     {
+        private string sourceWord;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Anagram"/> class.
         /// </summary>
@@ -12,7 +16,17 @@ namespace AnagramTask
         /// <exception cref="ArgumentException">Thrown when  source word is empty.</exception>
         public Anagram(string sourceWord)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (sourceWord is null)
+            {
+                throw new ArgumentNullException($"Thrown when source word is null. {nameof(sourceWord)}");
+            }
+
+            if (string.IsNullOrEmpty(sourceWord))
+            {
+                throw new ArgumentException($"Thrown when source word is empty.");
+            }
+
+            this.sourceWord = sourceWord;
         }
 
         /// <summary>
@@ -23,7 +37,50 @@ namespace AnagramTask
         /// <exception cref="ArgumentNullException">Thrown when candidates list is null.</exception>
         public string[] FindAnagrams(string[] candidates)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (candidates is null)
+            {
+                throw new ArgumentNullException($"Thrown when candidates list is null. {nameof(candidates)}");
+            }
+
+            List<string> result = new List<string>();
+            foreach (var str in candidates)
+            {
+                if (this.SearchInWord(str))
+                {
+                    result.Add(str);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public bool SearchInWord(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length != this.sourceWord.Length)
+            {
+                return false;
+            }
+
+            if (str.ToLower(CultureInfo.CurrentCulture) == this.sourceWord.ToLower(CultureInfo.CurrentCulture))
+            {
+                return false;
+            }
+
+            var strChar = str.ToLower(CultureInfo.CurrentCulture).ToCharArray();
+            var sourceChar = this.sourceWord.ToLower(CultureInfo.CurrentCulture).ToCharArray();
+
+            Array.Sort(strChar);
+            Array.Sort(sourceChar);
+
+            str = new string(strChar);
+            this.sourceWord = new string(sourceChar);
+
+            return str == this.sourceWord;
         }
     }
 }
